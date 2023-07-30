22f6f966bd7a109cc8ecd59309f02f9a5b54eadc
amozhegov <kaneImfb@gmail.com>
2023-07-30 17:38:21 +0200

    Finished form

--- app/currency_exchange/views.py
+++ app/currency_exchange/views.py
@@ -10,7 +10,26 @@ def exchange(request):
                'currencies': currencies
           }
 
-     return render(request=request, 
+          return render(request=request, 
+                   template_name='currency_exchange/index.html', 
+                   context=context)
+     
+     if request.method == 'POST':
+          from_amount = float(request.POST.get('from-amount'))
+          from_curr = request.POST.get('from-curr')
+          to_curr = request.POST.get('to-curr')
+          
+          converted_amount = round((currencies[to_curr] / currencies[from_curr]) * float(from_amount), 2)
+
+          context = {
+               'from_curr': from_curr,
+               'to_curr': to_curr,
+               'from_amount': from_amount,
+               'currencies': currencies,
+               'converted_amount': converted_amount,
+          }
+
+          return render(request=request, 
                    template_name='currency_exchange/index.html', 
                    context=context)
 
