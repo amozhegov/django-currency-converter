bb757effe2d2c923d3a765674dd7727b96cb1d05
amozhegov <kaneImfb@gmail.com>
2023-07-30 17:04:10 +0200

    Fixed calc form

--- app/currency_exchange/views.py
+++ app/currency_exchange/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+import requests
 
 def exchange(request):
-     name = 'Santa Claus'
+     response = requests.get(url='https://v6.exchangerate-api.com/v6/d42c804b648563db66c26634/latest/USD').json()
+     currencies = response.get('conversion_rates')
 
-     context = {
-          'name': name
-     }
+     if request.method == 'GET':
+          context = {
+               'currencies': currencies
+          }
+
      return render(request=request, 
                    template_name='currency_exchange/index.html', 
                    context=context)
